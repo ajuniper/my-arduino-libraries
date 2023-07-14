74fde4873547ea86608ef8258750fa776fcff583
ajuniper <ajuniper@local.local>
2023-07-14 22:14:48 +0100

    esp8266

--- mywifi.cpp
+++ mywifi.cpp
@@ -1,24 +1,40 @@
 // common wifi stuff
 #include <Arduino.h>
-#include <WiFi.h>
 #include <my_secrets.h>
 #include <mywifi.h>
 #include <mysyslog.h>
+
+#ifdef ESP8266
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
+#else
+
+#endif
+
 static const char* ssid     = MY_WIFI_SSID;
 static const char* password = MY_WIFI_PASSWORD;
 
 static int disconnecttime = 0;
 
+#ifdef ESP8266
+static void WiFiStationDisconnected(const WiFiEventStationModeDisconnected& event){
+    Serial.println("WiFi lost connection");
+#else
 static void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info){
     Serial.print("WiFi lost connection. Reason: ");
     Serial.println(info.wifi_sta_disconnected.reason);
+#endif
     if (disconnecttime == 0) {
         disconnecttime = time(NULL);
     }
     WiFi.reconnect();
 }
 
+#ifdef ESP8266
+static void WiFiStationConnected(const WiFiEventStationModeGotIP& event){
+#else
 static void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info){
+#endif
     if (disconnecttime != 0) {
         syslog.logf("Wifi connected again after %d seconds", time(NULL) - disconnecttime);
         disconnecttime = 0;
@@ -28,8 +44,13 @@ static void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info){
 void WIFI_init() {
     Serial.print("Connecting");
     WiFi.begin(ssid, password);
+#ifdef ESP8266
+    wifiConnectHandler = WiFi.onStationModeGotIP(WiFiStationConnected);
+    wifiDisconnectHandler = WiFi.onStationModeDisconnected(WiFiStationDisconnected);
+#else
     WiFi.onEvent(WiFiStationDisconnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
     WiFi.onEvent(WiFiStationConnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_CONNECTED);
+#endif
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
--- mywifi.h
+++ mywifi.h
@@ -1,2 +1,7 @@
 // wifi stuff
+#ifdef ESP8266
+#include <ESP8266WiFi.h>
+#else
+#include <WiFi.h>
+#endif
 extern void WIFI_init();
