39481648cf881cec84b5ccae1c417dfe74c97776
ajuniper <ajuniper@local.local>
2023-12-30 18:16:16 +0000

    add wrapper to avoid exporting syslog object
    helps with linkage and dependencies

--- mysyslog.cpp
+++ mysyslog.cpp
@@ -4,5 +4,23 @@
 #include <my_secrets.h>
 #include <WiFiUdp.h>
 static WiFiUDP udpClient;
-extern const char * syslog_name;
-Syslog syslog(udpClient, MY_SYSLOG_SERVER, 514, syslog_name, syslog_name, LOG_DAEMON);
+Syslog *syslog = NULL;
+void syslogf(uint16_t pri, const char *fmt, ...) {
+    if (syslog) {
+        va_list args;
+        va_start(args, fmt);
+        syslog->vlogf(pri, fmt, args);
+        va_end(args);
+    }
+}
+void syslogf(const char *fmt, ...) {
+    if (syslog) {
+        va_list args;
+        va_start(args, fmt);
+        syslog->vlogf(LOG_DAEMON, fmt, args);
+        va_end(args);
+    }
+}
+void SyslogInit(const char * name) {
+    syslog = new Syslog(udpClient, MY_SYSLOG_SERVER, 514, name, name, LOG_DAEMON);
+}
--- mysyslog.h
+++ mysyslog.h
@@ -1,3 +1,5 @@
 // syslog stuff
 #include <Syslog.h>
-extern Syslog syslog;
+extern void syslogf(uint16_t pri, const char *fmt, ...);
+extern void syslogf(const char *fmt, ...);
+extern void SyslogInit(const char * name);
